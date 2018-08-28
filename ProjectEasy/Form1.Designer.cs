30b9a51118fc2d01ebc1c7b3c2bb42958b6c1ec2
Paul Abel Sanchez <paul12p@hotmail.com>
2018-08-28 00:23:25 -0400

    Agregar archivos de proyecto.

---
+++ ProjectEasy/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace ProjectEasy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnopen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btntxt = new System.Windows.Forms.Button();
+            this.btnimg = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.txtfilas = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnfilas = new System.Windows.Forms.Button();
+            this.btnselect = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnfully = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnfolder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnopen
+            // 
+            this.btnopen.Location = new System.Drawing.Point(18, 60);
+            this.btnopen.Name = "btnopen";
+            this.btnopen.Size = new System.Drawing.Size(75, 23);
+            this.btnopen.TabIndex = 0;
+            this.btnopen.Text = "Open csv";
+            this.btnopen.UseVisualStyleBackColor = true;
+            this.btnopen.Click += new System.EventHandler(this.btnopen_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btntxt
+            // 
+            this.btntxt.Location = new System.Drawing.Point(81, 205);
+            this.btntxt.Name = "btntxt";
+            this.btntxt.Size = new System.Drawing.Size(75, 23);
+            this.btntxt.TabIndex = 2;
+            this.btntxt.Text = "Open text";
+            this.btntxt.UseVisualStyleBackColor = true;
+            this.btntxt.Click += new System.EventHandler(this.btntxt_Click);
+            // 
+            // btnimg
+            // 
+            this.btnimg.Location = new System.Drawing.Point(244, 205);
+            this.btnimg.Name = "btnimg";
+            this.btnimg.Size = new System.Drawing.Size(75, 23);
+            this.btnimg.TabIndex = 3;
+            this.btnimg.Text = "Open img";
+            this.btnimg.UseVisualStyleBackColor = true;
+            this.btnimg.Click += new System.EventHandler(this.btnimg_Click);
+            // 
+            // txtfilas
+            // 
+            this.txtfilas.Location = new System.Drawing.Point(168, 63);
+            this.txtfilas.Name = "txtfilas";
+            this.txtfilas.Size = new System.Drawing.Size(100, 20);
+            this.txtfilas.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Numero de filas";
+            // 
+            // btnfilas
+            // 
+            this.btnfilas.Location = new System.Drawing.Point(172, 103);
+            this.btnfilas.Name = "btnfilas";
+            this.btnfilas.Size = new System.Drawing.Size(75, 23);
+            this.btnfilas.TabIndex = 6;
+            this.btnfilas.Text = "OK";
+            this.btnfilas.UseVisualStyleBackColor = true;
+            this.btnfilas.Click += new System.EventHandler(this.btnfilas_Click);
+            // 
+            // btnselect
+            // 
+            this.btnselect.Location = new System.Drawing.Point(40, 143);
+            this.btnselect.Name = "btnselect";
+            this.btnselect.Size = new System.Drawing.Size(75, 23);
+            this.btnselect.TabIndex = 7;
+            this.btnselect.Text = "Open img";
+            this.btnselect.UseVisualStyleBackColor = true;
+            this.btnselect.Visible = false;
+            this.btnselect.Click += new System.EventHandler(this.btnselect_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "1er paso";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(346, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "2do paso";
+            // 
+            // btnfully
+            // 
+            this.btnfully.Location = new System.Drawing.Point(340, 124);
+            this.btnfully.Name = "btnfully";
+            this.btnfully.Size = new System.Drawing.Size(75, 23);
+            this.btnfully.TabIndex = 10;
+            this.btnfully.Text = "Fully";
+            this.btnfully.UseVisualStyleBackColor = true;
+            this.btnfully.Click += new System.EventHandler(this.btnfully_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(337, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Con img charged";
+            // 
+            // btnfolder
+            // 
+            this.btnfolder.Location = new System.Drawing.Point(340, 63);
+            this.btnfolder.Name = "btnfolder";
+            this.btnfolder.Size = new System.Drawing.Size(75, 23);
+            this.btnfolder.TabIndex = 12;
+            this.btnfolder.Text = "Open folder";
+            this.btnfolder.UseVisualStyleBackColor = true;
+            this.btnfolder.Click += new System.EventHandler(this.btnfolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 240);
+            this.Controls.Add(this.btnfolder);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnfully);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnselect);
+            this.Controls.Add(this.btnfilas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtfilas);
+            this.Controls.Add(this.btnimg);
+            this.Controls.Add(this.btntxt);
+            this.Controls.Add(this.btnopen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnopen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btntxt;
+        private System.Windows.Forms.Button btnimg;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox txtfilas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnfilas;
+        private System.Windows.Forms.Button btnselect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnfully;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnfolder;
+    }
+}
+
---
+++ ProjectEasy/Form1.cs
@@ -0,0 +1,334 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectEasy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int filas = 0;
+        public string[] listaimg = new string[1500];
+        Microsoft.Office.Interop.Excel.Application xlapp = new Microsoft.Office.Interop.Excel.Application();
+        Worksheet ws;
+        private void btnopen_Click(object sender, EventArgs e)
+        {
+            //abrir();
+            abierto();
+        }
+
+        void abrir()
+        {
+            Microsoft.Office.Interop.Excel.Application xlapp = new Microsoft.Office.Interop.Excel.Application();
+
+            if (xlapp == null)
+            {
+                Console.WriteLine("No puede ser abierto");
+                return;
+            }
+            xlapp.Visible = true;
+
+            Workbook wb = xlapp.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+            Worksheet ws = (Worksheet)wb.Worksheets[1];
+
+            if (ws == null)
+            {
+                Console.WriteLine("Worksheet no puede ser creado.");
+            }
+
+            Range range = ws.Range["C1", "C7"];
+            var n = ws.Range["C1"].Value2;
+
+            if (range == null)
+            {
+                Console.WriteLine("Rango no puede ser obtenido");
+            }
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF|" +
+                "All files (*.*)|*.*";
+
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.Title = "Select photos";
+
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            ws.Shapes.AddPicture(openFileDialog1.FileName, Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoCTrue, Left, Top, 50, 50);
+            Console.WriteLine(n);
+
+            /*
+            Object[] args = new Object[1];
+            args[0] = 6;
+            range.GetType().InvokeMember("Value", System.Reflection.BindingFlags.SetProperty, null, range, args);
+            range.Value2 = 8;*/
+
+        }
+
+        private void btntxt_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = @"C:\";
+
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.ShowReadOnly = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtfilas.Text = openFileDialog1.FileName;
+            }
+
+        }
+
+        private void btnimg_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool ThumbnailCallBack()
+        {
+            return false;
+        }
+
+        public string[] Imagenes()
+        {
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF|" +
+                "All files (*.*)|*.*";
+
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.Title = "Select photos";
+
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach (String file in openFileDialog1.FileNames)
+                {
+                    try
+                    {
+                        PictureBox image = new PictureBox();
+                        image.Height = 100;
+                        image.Width = 100;
+
+                        Image.GetThumbnailImageAbort myCallback =
+                            new Image.GetThumbnailImageAbort(ThumbnailCallBack);
+                        Bitmap bitmap = new Bitmap(file);
+                        Image myThumbnail = bitmap.GetThumbnailImage(96, 96, myCallback, IntPtr.Zero);
+                        image.Image = myThumbnail;
+
+                        Console.WriteLine(System.IO.Path.GetFileNameWithoutExtension(file));
+                        //PhotoGallary.Controls.Add(image);
+                    }
+                    catch (Exception ex)
+                    {
+
+                        MessageBox.Show("Error: " + ex.Message);
+                        string[] vacio = new string[100];
+                        return vacio;
+                    }
+                }
+            }
+            return openFileDialog1.FileNames;
+        }
+        void abierto()
+        {
+            openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string local = openFileDialog1.FileName;
+                String filename = DialogResult.ToString();
+
+                xlapp.Visible = true;
+                xlapp.Workbooks.Open(local);
+                ws = (Worksheet)xlapp.Worksheets[1];
+
+                Range range = ws.Range["C1", $"C50"];
+                var n = ws.Range["C1"].Value2;
+
+                if (range == null)
+                {
+                    Console.WriteLine("Rango no puede ser obtenido");
+                }
+            }
+
+        }
+
+        private void btnfilas_Click(object sender, EventArgs e)
+        {
+            filas = Convert.ToInt16(txtfilas.Text);
+        }
+
+        private void btnselect_Click(object sender, EventArgs e)
+        {
+
+            if (filas > 0)
+            {
+                openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.Filter = "Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF|" +
+                    "All files (*.*)|*.*";
+
+                openFileDialog1.Multiselect = true;
+                openFileDialog1.Title = "Select photos";
+
+                DialogResult dr = openFileDialog1.ShowDialog();
+                if (dr == System.Windows.Forms.DialogResult.OK)
+                {
+                    listaimg = openFileDialog1.FileNames;
+                    Range[] lista = new Range[filas];
+                    Range range2 = ws.Range["E1", $"E{filas}"];
+                    Range[] lista2 = new Range[filas];
+                    for (int i = 0; i <= listaimg.Length; i++)
+                    {
+                        for (int j = 1; j <= filas; j++)
+                        {
+                            try
+                            {
+                                if (System.IO.Path.GetFileNameWithoutExtension(listaimg[i]) == ws.Range[$"C{j}"].Text)
+                                {
+                                    Console.WriteLine("uno");
+                                    Microsoft.Office.Interop.Excel.Range oRange = (Microsoft.Office.Interop.Excel.Range)ws.Cells[j, 5];
+                                    float Left = (float)((double)oRange.Left);
+                                    float Top = (float)((double)oRange.Top);
+                                    const float ImageSize = 32;
+                                    ws.Shapes.AddPicture(listaimg[i], Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoCTrue, Left, Top, 70, 90);
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+
+                                MessageBox.Show("Error: " + ex.Message);
+                                break;
+                            }
+
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                //mm
+            }
+        }
+
+        private void btnfully_Click(object sender, EventArgs e)
+        {
+
+            if (filas > 0)
+            {
+
+                Range[] lista = new Range[filas];
+                Range range2 = ws.Range["E1", $"E{filas}"];
+                Range[] lista2 = new Range[filas];
+                for (int i = 0; i <= listaimg.Length; i++)
+                {
+                    for (int j = 1; j <= filas; j++)
+                    {
+                        try
+                        {
+                            if (System.IO.Path.GetFileNameWithoutExtension(listaimg[i]) == ws.Range[$"C{j}"].Text)
+                            {
+                                Console.WriteLine("uno");
+                                Microsoft.Office.Interop.Excel.Range oRange = (Microsoft.Office.Interop.Excel.Range)ws.Cells[j, 5];
+                                float Left = (float)((double)oRange.Left);
+                                float Top = (float)((double)oRange.Top);
+                                const float ImageSize = 32;
+                                ws.Shapes.AddPicture(listaimg[i], Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoCTrue, Left, Top, 70, 90);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+
+                            MessageBox.Show("Error: " + ex.Message);
+                            break;
+                        }
+                    }
+                }
+                //Microsoft.Office.Interop.Excel.Range oRange = (Microsoft.Office.Interop.Excel.Range)ws.Cells[3, 5];
+                //float Left = (float)((double)oRange.Left);
+                //float Top = (float)((double)oRange.Top);
+                // const float ImageSize = 32;
+                // ws.Shapes.AddPicture(@"C:\Users\Raul Sanchez\Desktop\2ca3e241-1407-433c-ae9b-96a1c03245d0.png", Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoCTrue, Left, Top, 70, 90);
+                //Console.WriteLine(lista[48].Text);
+                //Console.WriteLine(lista2[48].Text);
+            }
+            else
+            {
+                //mm
+            }
+        }
+
+        private void btnfolder_Click(object sender, EventArgs e)
+        {
+            if (filas > 0)
+            {
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    DialogResult result = fbd.ShowDialog();
+
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        string[] files = Directory.GetFiles(fbd.SelectedPath);
+                        DirectoryInfo dir = new DirectoryInfo(fbd.SelectedPath);
+                        int h = 0;
+                        foreach (var item in dir.GetFiles())
+                        {
+                            listaimg[h] = item.FullName;
+                            Console.WriteLine(item.FullName);
+                            string popo = System.IO.Path.GetFileNameWithoutExtension(item.FullName);
+                            Console.WriteLine(popo);
+                            h += 1;
+                        }
+                        Range[] lista = new Range[filas];
+                        Range range2 = ws.Range["E1", $"E{filas}"];
+                        Range[] lista2 = new Range[filas];
+                        for (int i = 0; i <= listaimg.Length; i++)
+                        {
+                            for (int j = 1; j <= filas; j++)
+                            {
+                                try
+                                {
+                                    if (System.IO.Path.GetFileNameWithoutExtension(listaimg[i]) == ws.Range[$"C{j}"].Text)
+                                    {
+                                        Console.WriteLine("uno");
+                                        Microsoft.Office.Interop.Excel.Range oRange = (Microsoft.Office.Interop.Excel.Range)ws.Cells[j, 5];
+                                        float Left = (float)((double)oRange.Left);
+                                        float Top = (float)((double)oRange.Top);
+                                        const float ImageSize = 32;
+                                        ws.Shapes.AddPicture(listaimg[i], Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoCTrue, Left, Top, 70, 90);
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+
+                                    MessageBox.Show("Error: " + ex.Message);
+                                    break;
+                                }
+
+                            }
+                        }
+                        //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                    }
+                }
+            }
+        }
+    }
+}
